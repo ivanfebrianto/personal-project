183a091d33d185af64019c803489afa4f864da42
if14618x <@Quinquaginta2!>
2026-01-30 19:56:55 +0700

    Initial Commit

---
+++ MooPics/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MooPics
+//
+//  Created by Ivan F on 30/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MooPics/MooPicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MooPicsApp.swift
+//  MooPics
+//
+//  Created by Ivan F on 30/01/26.
+//
+
+import SwiftUI
+
+@main
+struct MooPicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
